df5dd4fdc4a3ef56258a964e04fa8dc1d5958516
Byskull2000 <grajedadiego1@gmail.com>
2025-07-23 19:48:02 -0400

    Tipos para que no de error la libreria

--- src/app/pdf/pdf.component.ts
+++ src/app/pdf/pdf.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 @Component({
   selector: 'app-pdf',
   templateUrl: './pdf.component.html',
