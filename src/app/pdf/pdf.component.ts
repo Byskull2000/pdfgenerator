acfb6331eb0f358aae5b92f516b3e646030971f3
Byskull2000 <grajedadiego1@gmail.com>
2025-07-23 20:05:31 -0400

    Ahora muestra

--- src/app/pdf/pdf.component.ts
+++ src/app/pdf/pdf.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
-(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'pdf-maker',
